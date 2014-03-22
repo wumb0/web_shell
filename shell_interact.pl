0f80319dc6ff144027b484014f387e69bb535af1
cheddar <jmg2967@rit.edu>
2014-03-22 18:18:05 -0400

    Fixed URLEncode so it is shorter and more efficient, updated code to reflect change in the function

--- shell_interact.pl
+++ shell_interact.pl
@@ -58,119 +58,27 @@ sub StripTags{
 #   HEY IT WORKS OKAY?!?   #
 ############################
 sub cd{
-	my $input = $_[0];
-        URLEncode($input);
-        my $encpwd=$pwd;
-        URLEncode($encpwd);
+	my $input = URLEncode($_[0]);
+        my $encpwd = URLEncode($pwd);
 	$pwd = `$curl=%63%64%20$encpwd%3B$input%3B%70%77%64`;
 	StripTags($pwd);
 	$pwd =~ s/^\s+|\s+$//g;	
 }
 
 ############################
-# URL Encodes most of the  #
-# input. Excludes: A-F,    #
-# 0-9, % and ^ for now b/c #
-# they were casusing       #
-# issues with double       #
-# replacement              #
+# URL Encodes the input.   #
+# Takes a string as the    #
+# data to be encoded and   #
+# returns the URL encoded  #
+# string                   #
 ############################
 sub URLEncode{
-        $_[0] =~ s/ /%20/g;
-        $_[0] =~ s/!/%21/g;
-        $_[0] =~ s/"/%22/g;
-        $_[0] =~ s/#/%23/g;
-        $_[0] =~ s/\$/%24/g;
-        #$_[0] =~ s/%/%25/g;
-        $_[0] =~ s/&/%26/g;
-        $_[0] =~ s/'/%27/g;
-        $_[0] =~ s/\(/%28/g;
-        $_[0] =~ s/\)/%29/g;
-        $_[0] =~ s/\*/%2A/g;
-        $_[0] =~ s/\+/%2B/g;
-        $_[0] =~ s/,/%2C/g;
-        $_[0] =~ s/-/%2D/g;
-        $_[0] =~ s/\./%2E/g;
-        $_[0] =~ s/\//%2F/g;
-        #$_[0] =~ s/0/%30/g;
-        #$_[0] =~ s/1/%31/g;
-        #$_[0] =~ s/2/%32/g;
-        #$_[0] =~ s/3/%33/g;
-        #$_[0] =~ s/4/%34/g;
-        #$_[0] =~ s/5/%35/g;
-        #$_[0] =~ s/6/%36/g;
-        #$_[0] =~ s/7/%37/g;
-        #$_[0] =~ s/8/%38/g;
-        #$_[0] =~ s/9/%39/g;
-        $_[0] =~ s/:/%3A/g;
-        $_[0] =~ s/;/%3B/g;
-        $_[0] =~ s/</%3C/g;
-        $_[0] =~ s/=/%3D/g;
-        $_[0] =~ s/>/%3E/g;
-        $_[0] =~ s/\?/%3F/g;
-        $_[0] =~ s/@/%40/g;
-        #$_[0] =~ s/A/%41/g;
-        #$_[0] =~ s/B/%42/g;
-        #$_[0] =~ s/C/%43/g;
-        #$_[0] =~ s/D/%44/g;
-        #$_[0] =~ s/E/%45/g;
-        #$_[0] =~ s/F/%46/g;
-        $_[0] =~ s/G/%47/g;
-        $_[0] =~ s/H/%48/g;
-        $_[0] =~ s/I/%49/g;
-        $_[0] =~ s/J/%4A/g;
-        $_[0] =~ s/K/%4B/g;
-        $_[0] =~ s/L/%4C/g;
-        $_[0] =~ s/M/%4D/g;
-        $_[0] =~ s/N/%4E/g;
-        $_[0] =~ s/O/%4F/g;
-        $_[0] =~ s/P/%50/g;
-        $_[0] =~ s/Q/%51/g;
-        $_[0] =~ s/R/%52/g;
-        $_[0] =~ s/S/%53/g;
-        $_[0] =~ s/T/%54/g;
-        $_[0] =~ s/U/%55/g;
-        $_[0] =~ s/V/%56/g;
-        $_[0] =~ s/W/%57/g;
-        $_[0] =~ s/X/%58/g;
-        $_[0] =~ s/Y/%59/g;
-        $_[0] =~ s/Z/%5A/g;
-        $_[0] =~ s/\[/%5B/g;
-        $_[0] =~ s/\\/%5C/g;
-        $_[0] =~ s/\]/%5D/g;
-        #$_[0] =~ s/^/%5E/g;
-        $_[0] =~ s/_/%5F/g;
-        $_[0] =~ s/a/%61/g;
-        $_[0] =~ s/b/%62/g;
-        $_[0] =~ s/c/%63/g;
-        $_[0] =~ s/d/%64/g;
-        $_[0] =~ s/e/%65/g;
-        $_[0] =~ s/f/%66/g;
-        $_[0] =~ s/g/%67/g;
-        $_[0] =~ s/h/%68/g;
-        $_[0] =~ s/i/%69/g;
-        $_[0] =~ s/j/%6A/g;
-        $_[0] =~ s/k/%6B/g;
-        $_[0] =~ s/l/%6C/g;
-        $_[0] =~ s/m/%6D/g;
-        $_[0] =~ s/n/%6E/g;
-        $_[0] =~ s/o/%6F/g;
-        $_[0] =~ s/p/%70/g;
-        $_[0] =~ s/q/%71/g;
-        $_[0] =~ s/r/%72/g;
-        $_[0] =~ s/s/%73/g;
-        $_[0] =~ s/t/%74/g;
-        $_[0] =~ s/u/%75/g;
-        $_[0] =~ s/v/%76/g;
-        $_[0] =~ s/w/%77/g;
-        $_[0] =~ s/x/%78/g;
-        $_[0] =~ s/y/%79/g;
-        $_[0] =~ s/z/%7A/g;
-        $_[0] =~ s/{/%7B/g;
-        $_[0] =~ s/\|/%7C/g;
-        $_[0] =~ s/}/%7D/g;
-        $_[0] =~ s/~/%7E/g;
-        $_[0] =~ s/`/%80/g;
+    my $encodedstr = "";
+    chomp($_[0]);
+    foreach (split //, $_[0]) {
+        $encodedstr .= "%".unpack "H*", $_;
+    }
+    return "$encodedstr";
 }
 
 ############################
@@ -211,9 +119,8 @@ sub shell{
             print "debug: $input\n";
             print "debug: $curl=cd $pwd;$input 2>&1\n";
         }
-        URLEncode($input);
-        my $encpwd = $pwd;
-        URLEncode($encpwd);
+        $input = URLEncode($input);
+        my $encpwd = URLEncode($pwd);
         if ($debug){print "debug: $curl=%63%64%20$encpwd%3B$input%202%3E%261\n";}
 	foreach my $cmd (@parts){
 		if ($cmd =~ m/cd \S+/){
